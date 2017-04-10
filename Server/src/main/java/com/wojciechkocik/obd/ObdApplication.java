57239aad28f8ed9ff7d1bdad9f995658e6cca65b
Wojciech Kocik <wojciechkocik@hotmail.com>
2017-04-10 21:32:02 +0200

    server

---
+++ Server/src/main/java/com/wojciechkocik/obd/ObdApplication.java
@@ -0,0 +1,12 @@
+package com.wojciechkocik.obd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObdApplication.class, args);
+	}
+}
